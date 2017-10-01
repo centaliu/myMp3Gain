cc7380d022d00a75d92ef1ad0de06d2b72b2b8b5
Centa (Chung-Hung) Liu <centaliu@gmail.com>
2017-10-01 18:29:21 +0800

    move to H35

--- myMp3Gain/frmMain.Designer.cs
+++ myMp3Gain/frmMain.Designer.cs
@@ -28,66 +28,66 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.txtPath = new System.Windows.Forms.TextBox();
-			this.lblPath = new System.Windows.Forms.Label();
-			this.btnBrowse = new System.Windows.Forms.Button();
-			this.btnGo = new System.Windows.Forms.Button();
-			this.folderDlg = new System.Windows.Forms.FolderBrowserDialog();
-			this.SuspendLayout();
-			// 
-			// txtPath
-			// 
-			this.txtPath.Location = new System.Drawing.Point(50, 11);
-			this.txtPath.Name = "txtPath";
-			this.txtPath.ReadOnly = true;
-			this.txtPath.Size = new System.Drawing.Size(324, 20);
-			this.txtPath.TabIndex = 0;
-			this.txtPath.Text = "C:\\Users\\Centa\\Videos\\Any Audio Converter\\MP3";
-			// 
-			// lblPath
-			// 
-			this.lblPath.AutoSize = true;
-			this.lblPath.Location = new System.Drawing.Point(12, 18);
-			this.lblPath.Name = "lblPath";
-			this.lblPath.Size = new System.Drawing.Size(32, 13);
-			this.lblPath.TabIndex = 1;
-			this.lblPath.Text = "Path:";
-			// 
-			// btnBrowse
-			// 
-			this.btnBrowse.Location = new System.Drawing.Point(380, 10);
-			this.btnBrowse.Name = "btnBrowse";
-			this.btnBrowse.Size = new System.Drawing.Size(28, 21);
-			this.btnBrowse.TabIndex = 2;
-			this.btnBrowse.Text = "...";
-			this.btnBrowse.UseVisualStyleBackColor = true;
-			this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
-			// 
-			// btnGo
-			// 
-			this.btnGo.Location = new System.Drawing.Point(414, 10);
-			this.btnGo.Name = "btnGo";
-			this.btnGo.Size = new System.Drawing.Size(40, 21);
-			this.btnGo.TabIndex = 3;
-			this.btnGo.Text = "Go";
-			this.btnGo.UseVisualStyleBackColor = true;
-			this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
-			// 
-			// frmMain
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(458, 47);
-			this.Controls.Add(this.btnGo);
-			this.Controls.Add(this.btnBrowse);
-			this.Controls.Add(this.lblPath);
-			this.Controls.Add(this.txtPath);
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Name = "frmMain";
-			this.Text = "myMp3Gain";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.folderDlg = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(50, 11);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(324, 20);
+            this.txtPath.TabIndex = 0;
+            this.txtPath.Text = "C:\\Users\\H35\\Videos\\Any Audio Converter\\MP3";
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(12, 18);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(32, 13);
+            this.lblPath.TabIndex = 1;
+            this.lblPath.Text = "Path:";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(380, 10);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(28, 21);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(414, 10);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(40, 21);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 47);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.txtPath);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "myMp3Gain";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
